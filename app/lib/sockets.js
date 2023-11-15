6731f4a0e9c18a944bec8cad14160167b42d6ec5
Robert Stocks <robert@avius.com>
2023-11-15 13:24:24 +0000

    typo

--- app/lib/sockets.js
+++ app/lib/sockets.js
@@ -7,7 +7,7 @@ let clients = []
 
 const func = {
   init: server => {
-    console.log('init connection client with host:', stripIPv6(server))
+    console.log('init connection client with host:', server)
     io = require('socket.io')(server)
     io.on('connection', client => {
       console.log(
